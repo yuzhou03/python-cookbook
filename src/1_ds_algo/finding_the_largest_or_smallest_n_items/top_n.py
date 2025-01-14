6229ffd49f2ce1bdc2aa0f1f6e7185ab27350750
zhouyu <yuzhou03@qq.com>
2025-01-14 11:28:20 +0800

    refine top-n

--- src/1_ds_algo/finding_the_largest_or_smallest_n_items/top_n.py
+++ src/1_ds_algo/finding_the_largest_or_smallest_n_items/top_n.py
@@ -4,17 +4,35 @@
 
 import heapq
 
+
+def delimiter():
+    print("-" * 100)
+
+
 portfolio = [
-   {'name': 'IBM', 'shares': 100, 'price': 91.1},
-   {'name': 'AAPL', 'shares': 50, 'price': 543.22},
-   {'name': 'FB', 'shares': 200, 'price': 21.09},
-   {'name': 'HPQ', 'shares': 35, 'price': 31.75},
-   {'name': 'YHOO', 'shares': 45, 'price': 16.35},
-   {'name': 'ACME', 'shares': 75, 'price': 115.65}
+    {"name": "IBM", "shares": 100, "price": 91.1},
+    {"name": "AAPL", "shares": 50, "price": 543.22},
+    {"name": "FB", "shares": 200, "price": 21.09},
+    {"name": "HPQ", "shares": 35, "price": 31.75},
+    {"name": "YHOO", "shares": 45, "price": 16.35},
+    {"name": "ACME", "shares": 75, "price": 115.65},
 ]
 
-cheap = heapq.nsmallest(3, portfolio, key=lambda s: s['price'])
-expensive = heapq.nlargest(3, portfolio, key=lambda s: s['price'])
+cheap = heapq.nsmallest(3, portfolio, key=lambda s: s["price"])
+expensive = heapq.nlargest(3, portfolio, key=lambda s: s["price"])
 
-print(cheap)
-print(expensive)
+print("cheap", cheap)
+print("expensive", expensive)
+
+delimiter()
+max_money = heapq.nlargest(1, portfolio, key=lambda s: s["price"] * s["shares"])
+min_money = heapq.nsmallest(1, portfolio, key=lambda s: s["price"] * s["shares"])
+
+print("max_money invested in ", max_money)
+print("min_money invested in ", min_money)
+delimiter()
+
+max_share = heapq.nlargest(2, portfolio, key=lambda s: s["shares"])
+min_share = heapq.nsmallest(2, portfolio, key=lambda s: s["shares"])
+print("max_share", max_share)
+print("min_share", min_share)
