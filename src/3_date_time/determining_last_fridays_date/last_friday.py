ee0f7e8f94d98488778cc7fef14c7a5f65928c1b
zhouyu <yuzhou03@qq.com>
2025-01-17 21:49:36 +0800

    last_friday

--- src/3_date_time/determining_last_fridays_date/last_friday.py
+++ src/3_date_time/determining_last_fridays_date/last_friday.py
@@ -1,15 +1,36 @@
 from datetime import datetime, timedelta
+from common.date import format_datetime, format_datestr
+from common.seperator import delimiter
 
-weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+weekdays = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+]
+
 
 def get_previous_byday(dayname, start_date=None):
     if start_date is None:
         start_date = datetime.today()
     day_num = start_date.weekday()
     day_num_target = weekdays.index(dayname)
-    days_ago = (7 + day_num - day_num_target) % 7 
+    days_ago = (7 + day_num - day_num_target) % 7
     if days_ago == 0:
         days_ago = 7
     target_date = start_date - timedelta(days=days_ago)
     return target_date
 
+
+print("today is ", format_datetime(datetime.now(), fmt="%Y-%m-%d"))
+print("now is ", format_datetime(datetime.now()))
+delimiter()
+
+for day in weekdays:
+    last_day = get_previous_byday(day)
+    print(
+        f"last {day:10} is {format_datetime(get_previous_byday(day), fmt='%Y-%m-%d')}"
+    )
