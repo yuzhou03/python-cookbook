b0ae76b6a893a32a3f048932ac7c7c5129b77209
zhouyu <yuzhou03@qq.com>
2025-01-19 22:16:52 +0800

    refine replace

--- src/2_string_text/searching_and_replacing_text/replace.py
+++ src/2_string_text/searching_and_replacing_text/replace.py
@@ -3,14 +3,20 @@
 # Examples of simple regular expression substitution
 
 import re
+from common.text import print_pad
 
 # Some sample text
-text = 'Today is 11/27/2012. PyCon starts 3/13/2013.'
+text = "Today is 11/27/2012. PyCon starts 3/13/2013."
+print_pad("Some sample text")
+print(text)
 
-datepat = re.compile(r'(\d+)/(\d+)/(\d+)')
+
+datepat = re.compile(r"(\d+)/(\d+)/(\d+)")
 
 # (a) Simple substitution
-print(datepat.sub(r'\3-\1-\2', text))
+print_pad("Simple substitution")
+print("replace date string. format: yyyy-mm-dd")
+print(datepat.sub(r"\3-\1-\2", text))
 
 # (b) Replacement function
 from calendar import month_abbr
@@ -19,15 +25,22 @@ from calendar import month_abbr
 def change_date(m):
     # month_abbr: Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec
     mon_name = month_abbr[int(m.group(1))]
-    return '{} {} {}'.format(m.group(2), mon_name, m.group(3))
+    return "{} {} {}".format(m.group(2), mon_name, m.group(3))
 
 
+print_pad("Replacement function")
 print("replaced date string:", datepat.sub(change_date, text))
 
 
 def show_month_abbr():
-    for i in range(13):
-        print(month_abbr[i])
+    """
+    打印1到12月的英文缩写。
+
+    该函数遍历从1到12的整数，并使用calendar模块中的month_abbr列表来打印每个月的英文缩写。
+    """
+    for i in range(1, 13):
+        # 打印当前月份的数字和对应的英文缩写
+        print(i, month_abbr[i])
 
 
 show_month_abbr()
