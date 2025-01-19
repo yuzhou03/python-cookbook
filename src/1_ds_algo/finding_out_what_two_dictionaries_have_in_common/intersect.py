02051be2111d4100765a2a67767b1c051ae78fd2
zhouyu <yuzhou03@qq.com>
2025-01-19 18:04:12 +0800

    refine comment

--- src/1_ds_algo/finding_out_what_two_dictionaries_have_in_common/intersect.py
+++ src/1_ds_algo/finding_out_what_two_dictionaries_have_in_common/intersect.py
@@ -1,10 +1,14 @@
 # example.py
 #
 # Find out what two dictionaries have in common
+from common.text import print_pad
 
 a = {"x": 1, "y": 2, "z": 3}
 b = {"w": 10, "x": 11, "y": 2}
-
+print_pad("Find out what two dictionaries have in common")
+print("a", a)
+print("b", b)
+print_pad("Result")
 # Common keys: {'y', 'x'}
 print("Common keys:", a.keys() & b.keys())
 # Keys in a not in b: {'z'}
--- src/2_string_text/specifying_a_regular_expression_for_the_shortest_match/regexp_lazy_match.py
+++ src/2_string_text/specifying_a_regular_expression_for_the_shortest_match/regexp_lazy_match.py
@@ -4,13 +4,29 @@
 
 import re
 
+from common.text import print_pad
+
 # Sample text
 text = 'Computer says "no." Phone says "yes."'
 
 # (a) Regex that finds quoted strings - longest match
-str_pat = re.compile(r'\"(.*)\"')
-print(str_pat.findall(text))  # ['no." Phone says "yes.']
+print_pad("longest match")
+"""
+代码中的正则表达式r"\"(.*)\""用于匹配双引号内的任意字符序列。这里的r表示原始字符串
+\"匹配双引号
+(.*) 是一个捕获组，匹配任意字符序列（除了换行符）。. 匹配除换行符以外的任意字符，* 表示匹配前面的子表达式零次或多次。
+由于没有使用非贪婪模式（即没有在 * 后面加上 ?），这个正则表达式会尽可能多地匹配字符，直到遇到最后一个双引号。
+"""
+str_pat = re.compile(r"\"(.*)\"")
+print("pattern", str_pat.pattern)
+
+print("matches(greedy-mode)", str_pat.findall(text))  # ['no." Phone says "yes.']
 
 # (b) Regex that finds quoted strings - shortest match
-str_pat = re.compile(r'\"(.*?)\"')
-print(str_pat.findall(text))  # ['no.', 'yes.']
+print_pad("shortest match")
+"""
+(.*?)匹配任意字符序列（除了换行符），并且使用了非贪婪模式（即最短匹配）。
+"""
+str_pat = re.compile(r"\"(.*?)\"")
+print("pattern", str_pat.pattern)
+print("matches(non-greedy mode)", str_pat.findall(text))  # ['no.', 'yes.']
