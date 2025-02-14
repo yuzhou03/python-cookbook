1e0af523bf45e39231cd5b6f3d9139a947d77623
Zhou Yu <yuzhou03@qq.com>
2025-02-14 18:07:33 +0800

    refine print

--- src/9_meta_programming/unwrapping_a_decorator/example.py
+++ src/9_meta_programming/unwrapping_a_decorator/example.py
@@ -1,6 +1,8 @@
 # Example of unwrapping a decorator
 
 from functools import wraps
+from common.text import print_pad
+
 
 def decorator1(func):
     @wraps(func)
@@ -9,6 +11,7 @@ def decorator1(func):
         return func(*args, **kwargs)
     return wrapper
 
+
 def decorator2(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
@@ -16,13 +19,23 @@ def decorator2(func):
         return func(*args, **kwargs)
     return wrapper
 
+
 @decorator1
 @decorator2
 def add(x, y):
     return x + y
 
+
 # Calling wrapped function
-print(add(2,3))
+print_pad('Calling wrapped function')
+print(add(2, 3))
+
 
 # Calling original function
-print(add.__wrapped__(2,3))
+print_pad('Calling original function. The original function is the one that is wrapped by the innermost decorator', size=120)
+print(add.__wrapped__(2, 3))
+
+
+print_pad(
+    'Calling the original function of the original function. with twice __wrapped__', size=120)
+print(add.__wrapped__.__wrapped__(2, 3))
